c6e2487a7d840063c7cd19af15dbd33b751086e8
kogleee <137287221+kogleee@users.noreply.github.com>
2023-11-17 16:19:40 +0300

    Add files via upload

--- kor/main.py
+++ kor/main.py
@@ -13,20 +13,19 @@ lab = [
 
 
 def BFS(start, end, lab):
-    current = start
-    visited = [[1, 3]]
-    stack =[]
+    current = start # текущее положение
+    visited = [start] # посещённые
+    stack =[] # очередь
 
+    #поиск соседних элементов с текущим положением
     while end not in visited:
-
         for i in range(current[0] - 1, current[0] + 2):
             for j in range(current[1] - 1, current[1] + 2):
                 if lab[i][j] == 0:
                     if ([i,j] not in stack) and ([i,j] not in visited):
                         stack.append([i,j])
 
-
-
+        #переход на первый элемент в очереди, добавление в посещённые и удаление из очереди
         current = stack[0]
         visited.append(stack[0])
         stack.remove(stack[0])
